3f928690f235a49b08476fac0ca7aaca862b8a94
mrstain <ferenc.matej@gmail.com>
2015-12-20 19:10:33 +0100

    preset fix && .fog-example

--- app/models/preset.rb
+++ app/models/preset.rb
@@ -1,8 +1,13 @@
 class Preset < ActiveRecord::Base
   has_many :instances
+  #before_save :check_preset
 
   validates :name, presence: true
   validates :flavor_id, presence: true
   validates :image_id, presence: true
   
+  private
+    def check_preset
+      PresetService.check(:image_id, :flavor_id)
+    end
 end
