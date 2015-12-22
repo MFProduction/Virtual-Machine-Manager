16f6fa36c881df4252edcd42a87ba0eed2c85bc9
mrstain <ferenc.matej@gmail.com>
2015-12-22 16:40:23 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-microUbuntuPreset = Preset.new(name:"Micro Ubuntu Server", image_id:"ami-5189a661", flavor_id:"t2.micro") 
-microRHELPreset = Preset.new(name:"Micro RHEL Server", image_id:"ami-5189a661", flavor_id:"t2.micro") 
+microUbuntuPreset = Preset.create(name:"Micro Ubuntu Server", image_id:"ami-5189a661", flavor_id:"t2.micro") 
+microRHELPreset = Preset.create(name:"Micro RHEL Server", image_id:"ami-5189a661", flavor_id:"t2.micro") 
